67db8d5194ad24f29cd66b5f06ecac2201f31ea3
i22s0616 <i22s0616@students.it-college.ru>
2023-12-22 10:28:44 +0500

    02 finish

--- 02_Number_guessing_game/script.js
+++ 02_Number_guessing_game/script.js
@@ -1,6 +1,7 @@
 const button = document.querySelector("button");
 const container = document.querySelector(".container");
 const counter = document.querySelector(".counter");
+const lastPunct = document.querySelector(".lastPunct");
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 let numberCheck = 1;
 
@@ -19,12 +20,14 @@ button.addEventListener("click", function (event) {
             container.insertAdjacentHTML("beforeend", `<div class="test"><p>Вы близки! Увеличте число</p></div>`);
             counter.innerHTML = "";
             counter.insertAdjacentHTML("beforeend", `<p class="counterP">Количество попыток: ${(10 - numberCheck)}</p>`);
+            lastPunct.insertAdjacentHTML("beforeend", `${input} `);
             numberCheck++;
         } else if (randomNumber < input && numberCheck <= 10) {
             container.innerHTML = "";
             container.insertAdjacentHTML("beforeend", `<div class="test"><p>Вы близки! Уменьшите число</p></div>`);
             counter.innerHTML = "";
             counter.insertAdjacentHTML("beforeend", `<p class="counterP">Количество попыток: ${(10 - numberCheck)}</p>`);
+            lastPunct.insertAdjacentHTML("beforeend", `${input} `);
             numberCheck++;
         } else if (numberCheck >= 11) {
             container.innerHTML = "";
