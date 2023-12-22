278d0052fbc2a61226619465c14c84c4089def1f
i22s0616 <i22s0616@students.it-college.ru>
2023-12-22 10:19:09 +0500

    02

---
+++ 02_Number_guessing_game/script.js
@@ -0,0 +1,34 @@
+const button = document.querySelector("button");
+const container = document.querySelector(".container");
+const counter = document.querySelector(".counter");
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let numberCheck = 1;
+
+
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+    const input = Number(document.querySelector("input").value);
+    if (input == 0) {
+        container.innerHTML = "";
+    } else {
+        if (randomNumber == input && numberCheck <= 10) {
+            container.innerHTML = "";
+            container.insertAdjacentHTML("beforeend", `<div class="result"><p>Вы победили!</p></div>`);
+        } else if (randomNumber > input && numberCheck <= 10) {
+            container.innerHTML = "";
+            container.insertAdjacentHTML("beforeend", `<div class="test"><p>Вы близки! Увеличте число</p></div>`);
+            counter.innerHTML = "";
+            counter.insertAdjacentHTML("beforeend", `<p class="counterP">Количество попыток: ${(10 - numberCheck)}</p>`);
+            numberCheck++;
+        } else if (randomNumber < input && numberCheck <= 10) {
+            container.innerHTML = "";
+            container.insertAdjacentHTML("beforeend", `<div class="test"><p>Вы близки! Уменьшите число</p></div>`);
+            counter.innerHTML = "";
+            counter.insertAdjacentHTML("beforeend", `<p class="counterP">Количество попыток: ${(10 - numberCheck)}</p>`);
+            numberCheck++;
+        } else if (numberCheck >= 11) {
+            container.innerHTML = "";
+            container.insertAdjacentHTML("beforeend", `<div class="fall"><p>Вы проиграли!</p></div>`);
+        }
+    }
+})
